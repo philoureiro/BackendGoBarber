339894e7eb9bdd5a901d65d27a865f44bb119053
Philipe Loureiro <75145548+philoureiro@users.noreply.github.com>
2021-01-09 13:53:58 -0300

    Primeira versão do backend do projeto.

---
+++ src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+    jwt: {
+        secret: '36342188c18536cfa460b6f9ae51c1e1',
+        expiresIn: '1d',
+    }
+};
---
+++ src/database/migrations/1610078602313-AddAvatarFieldToUsers.ts
@@ -0,0 +1,19 @@
+import { MigrationInterface, QueryRunner, Table, TableColumn } from 'typeorm';
+
+export default class AddAvatarFieldToUsers1610078602313
+    implements MigrationInterface {
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.addColumn(
+            'users',
+            new TableColumn({
+                name: 'varchar',
+                type: 'varchar',
+                isNullable: true,
+            }),
+        );
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropColumn('users', 'avatar');
+    }
+}
